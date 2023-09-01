e440c9d66fe07b0f6c6b0d1bcbc17653d78fb9a2
WilliamNB <59480886+WilliamNB@users.noreply.github.com>
2023-09-01 10:50:55 +0100

    WRITTEN OFF

--- src/components/results/ResultsOverview.js
+++ src/components/results/ResultsOverview.js
@@ -3,9 +3,10 @@ import React from 'react'
 const ResultsOverview = ({results}) => {
 const finalResult = results[results.length - 1];
 console.log(finalResult);
+
   return (
     <div class="row">
-        <div class="col-sm-4 overview">Years to pay off loan: {results.length}</div>
+        <div class="col-sm-4 overview">Years to pay off loan: {finalResult.remainingBalance > 0 ? results.length+" (Written Off)" : results.length}</div>
         <div class="col-sm-4 overview">Total Paid: £{Math.round(finalResult.totalPaid)}</div>
         <div class="col-sm-4 overview">Total Interest Payed: £{Math.round(finalResult.paidInterestTotal)}</div>
     </div>
